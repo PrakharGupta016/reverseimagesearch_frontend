4a0117cf78f3509f19d0c003f4196bed85b88fde
PrakharGupta016 <86834271+PrakharGupta016@users.noreply.github.com>
2023-10-01 03:08:34 +0530

    added the post api

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,41 +1,39 @@
 import { useState } from "react";
 
+const HomePage = () => {
+  const [fileList, setFileList] = useState([]);
+  const [fileUrl, setFileUrl] = useState(null);
+  const [file, setFile] = useState(null);
+  const handleFileChange = (e) => {
+    const uploadedFile = e.target.files[0];
+    setFile(uploadedFile);
+    const url = URL.createObjectURL(uploadedFile);
+    setFileUrl(url);
+  };
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
 
-
-
-const handleOnSubmit = (e)=>{
-  e.preventDefault();
-  fetch('https://jsonplaceholder.typicode.com/posts', {
-    method: 'POST',
-    body: JSON.stringify({
-      title: 'foo',
-      body: 'bar',
-      userId: 1,
-    }),
-    headers: {
-      'Content-type': 'application/json; charset=UTF-8',
-    },
-  })
-    .then((response) => response.json())
-    .then((json) => console.log(json));
-}
-
-const HomePage = ()=>{
-  const [fileList,setFileList] = useState([])
+    if (file) {
+      const formData = new FormData();
+      formData.append("file", file);
+      console.log(file)
+      fetch("http://35.200.146.98:8000/search", {
+        method: "POST",
+        body: formData,
+      })
+        .then((response) => response.json())
+        .then((data) => setFileList(data[0]));
+    }
+  };
   return (
-      <div>
-          <form onSubmit={handleOnSubmit}>
-
-              <input type="file"/>  
-              <button type = "submit">submit</button>
-
-          </form>
-
-      </div>
-
-
-
+    <div>
+      <form onSubmit={handleOnSubmit}>
+        <input type="file" onChange={handleFileChange} />
+        <button type="submit">submit</button>
+      </form>
+    </div>
+    
   );
-}
+};
 
 export default HomePage;
